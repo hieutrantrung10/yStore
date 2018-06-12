dea7333697bf7dff8512d223decd7ea2d44f6521
DESKTOP-JL2TPFM\Hieu <hieutrantrung10@gmail.com>
2018-06-12 12:15:06 +0700

    create infrastructure

---
+++ yStore.Data/Infrastructure/Disposable.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yStore.Data.Infrastructure
+{
+    public class Disposable : IDisposable
+    {
+        private bool isDispose;
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if(!isDispose && disposing)
+            {
+                DisposeCore();
+            }
+            isDispose = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+
+        }
+    }
+}
